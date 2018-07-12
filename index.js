7d7dcb0461812217ad4c55b8aa98edf986f8a5c8
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 14:29:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function printBadges (names)
 var gifts =['ava','john','will','claire'];
 for (var counter = 0; counter < printBadges.length; counter++){
-  console.log(`Welcome ${names[counter]}! You are employee #${counter + 1)
+  console.log(`Welcome ${names[counter]}! You are employee #${counter + 1})
   printBadges(gift)
 }
