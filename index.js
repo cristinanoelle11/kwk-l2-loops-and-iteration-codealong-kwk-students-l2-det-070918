04949f2e6682a559002aafad371ca6141b388f16
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 14:25:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-function printBadges(['ava','john','will','claire']);
+function printBadges = (['ava','john','will','claire']);
 for (var counter = 0; counter < printBadges.length; counter++){
   var gift = printBadges[counter]
   printBadges(gift)
