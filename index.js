971a195dbdebe438da528ab2beb7a43c86f3a68b
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 14:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges (names){
-for (var counter = 0; counter < printBadges.length; counter++){
+for (var counter = 0; counter < names.length; counter++){
   console.log(`Welcome ${names[counter]}! You are employee #${counter + 1}.`);}
 }
 return names
