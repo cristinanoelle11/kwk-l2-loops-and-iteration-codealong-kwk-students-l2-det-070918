c053e05c183ba5be9f6b38ecc8eb87903af79978
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 14:24:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Code your solutions in this file
 function printBadges(['ava','john','will','claire']);
+for (var counter = 0; counter < printBadges.length; counter++){
+  var gift = printBadges[counter]
+  wrapGift(gift)
