1d854bb4d5f33650be5f5015a2b53b5a35a1c2fd
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 14:28:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function printBadges (names)
 var gifts =['ava','john','will','claire'];
 for (var counter = 0; counter < printBadges.length; counter++){
-  var gift = gifts[counter]
+  
   printBadges(gift)
 }
